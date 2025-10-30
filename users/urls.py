4011c42198e1d6324f77dbec99122413d003ae8a
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-10-30 18:36:10 +0530

    update

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path,include
-from users import views
+from users.views import RegisterView
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -8,6 +8,7 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
+    path('api/auth/register/',RegisterView.as_view(),name='auth_register'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer,LginSerializer
 
 # Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = RegisterSerializer
+
